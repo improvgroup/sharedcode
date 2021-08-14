63ae0e76d6e18c1f279e192792b101db254755af
William Forney <bill@improvgroup.com>
2021-08-13 21:45:35 -0700

    Update safe fire and forget methods.

--- SharedCode.Core/Threading/Tasks/ValueTaskExtensions.cs
+++ SharedCode.Core/Threading/Tasks/ValueTaskExtensions.cs
@@ -1,28 +1,28 @@
+using System;
+using System.Threading.Tasks;
+
 namespace SharedCode.Threading.Tasks
 {
-	using System;
-	using System.Threading.Tasks;
-
 	/// <summary>
-	/// The extension methods class for <see cref="Task"/>.
+	/// The extension methods class for <see cref="ValueTask" />.
 	/// </summary>
-	public static class TaskExtensions
+	public static class ValueTaskExtensions
 	{
 		/// <summary>
-		/// Safely execute the <see cref="Task"/> without waiting for it to complete before moving to the next line of code; commonly known as "Fire And Forget".
+		/// Safely execute the <see cref="ValueTask"/> without waiting for it to complete before moving to the next line of code; commonly known as "Fire And Forget".
 		/// </summary>
-		/// <param name="task">The <see cref="Task"/>.</param>
+		/// <param name="task">The <see cref="ValueTask"/>.</param>
 		/// <param name="continueOnCapturedContext">
 		/// If set to <c>true</c> continue on captured context; this will ensure that the Synchronization Context returns to the calling thread. If set to <c>false</c> continue on a different context; this will allow the Synchronization Context to continue on a different thread.
 		/// </param>
 		/// <param name="onException">
-		/// If an exception is thrown in the <see cref="Task"/>, <c>onException</c> will execute. If onException is null, the exception will be re-thrown.
+		/// If an exception is thrown in the <see cref="ValueTask"/>, <c>onException</c> will execute. If onException is null, the exception will be re-thrown.
 		/// </param>
 		/// <remarks>
 		/// Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.
 		/// </remarks>
 		public static async void SafeFireAndForgetAsync<TException>(
-			this Task task,
+			this ValueTask task,
 			bool continueOnCapturedContext = true,
 			Action<TException>? onException = null)
 			where TException : Exception
