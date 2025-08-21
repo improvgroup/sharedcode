1bc597143a07c03af61673413ea3a6c53a447f2d
William Forney <william@williamforney.com>
2025-08-20 22:51:34 -0700

    Improve documentation and safety in async methods
    Updated documentation comments for `SafeFireAndForgetAsync` methods in `TaskExtensions.cs` and `ValueTaskExtensions.cs` for clarity and consistency. Enhanced null checks using `ArgumentNullException.ThrowIfNull(task)` for .NET 6.0 or greater. Overloaded methods to support both generic and non-generic exception handling, improving code quality and maintainability.

--- SharedCode.Core/Threading/Tasks/TaskExtensions.cs
+++ SharedCode.Core/Threading/Tasks/TaskExtensions.cs
@@ -1,68 +1,87 @@
 ﻿namespace SharedCode.Threading.Tasks;
+
 /// <summary>
-/// The extension methods class for <see cref="Task" />.
+/// The extension methods class for <see cref="Task"/>.
 /// </summary>
+[SuppressMessage(
+    "Usage",
+    "GCop500:Void async methods cannot be awaited. Also they can hide exceptions and cause tricky bugs. Return a Task instead.",
+    Justification = "The whole point is to fire and forget without awaiting.")]
 public static class TaskExtensions
 {
-	/// <summary>
-	/// Safely execute the <see cref="Task" /> without waiting for it to complete before moving
-	/// to the next line of code; commonly known as "Fire And Forget".
-	/// </summary>
-	/// <typeparam name="TException">The type of exception.</typeparam>
-	/// <param name="task">The <see cref="Task" />.</param>
-	/// <param name="continueOnCapturedContext">
-	/// If set to <c>true</c> continue on captured context; this will ensure that the
-	/// Synchronization Context returns to the calling thread. If set to <c>false</c> continue
-	/// on a different context; this will allow the Synchronization Context to continue on a
-	/// different thread.
-	/// </param>
-	/// <param name="onException">
-	/// If an exception is thrown in the <see cref="Task" />, <c>onException</c> will execute.
-	/// If onException is null, the exception will be re-thrown.
-	/// </param>
-	/// <remarks>Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.</remarks>
-	/// <exception cref="ArgumentNullException">task</exception>
-	public static async void SafeFireAndForgetAsync<TException>(this Task task, bool continueOnCapturedContext = true, Action<TException>? onException = null)
-		where TException : Exception
-	{
-		try
-		{
-			_ = task ?? throw new ArgumentNullException(nameof(task));
-			await task.ConfigureAwait(continueOnCapturedContext);
-		}
-		catch (TException ex) when (onException is not null)
-		{
-			onException.Invoke(ex);
-		}
-	}
+    /// <summary>
+    /// Safely execute the <see cref="Task"/> without waiting for it to complete before moving to
+    /// the next line of code; commonly known as "Fire And Forget".
+    /// </summary>
+    /// <typeparam name="TException">The type of exception.</typeparam>
+    /// <param name="task">The <see cref="Task"/>.</param>
+    /// <param name="continueOnCapturedContext">
+    /// If set to <c>true</c> continue on captured context; this will ensure that the
+    /// Synchronization Context returns to the calling thread. If set to <c>false</c> continue on a
+    /// different context; this will allow the Synchronization Context to continue on a different thread.
+    /// </param>
+    /// <param name="onException">
+    /// If an exception is thrown in the <see cref="Task"/>, <c>onException</c> will execute. If
+    /// onException is null, the exception will be re-thrown.
+    /// </param>
+    /// <remarks>Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.</remarks>
+    /// <exception cref="ArgumentNullException">task</exception>
+    public static async void SafeFireAndForgetAsync<TException>(this Task task, bool continueOnCapturedContext = true, Action<TException>? onException = null)
+        where TException : Exception
+    {
+        try
+        {
+#if NET6_0_OR_GREATER
+            ArgumentNullException.ThrowIfNull(task);
+#else
+            if (task is null)
+            {
+                throw new ArgumentNullException(nameof(task));
+            }
+#endif
 
-	/// <summary>
-	/// Safely execute the <see cref="Task" /> without waiting for it to complete before moving
-	/// to the next line of code; commonly known as "Fire And Forget".
-	/// </summary>
-	/// <param name="task">The <see cref="Task" />.</param>
-	/// <param name="continueOnCapturedContext">
-	/// If set to <c>true</c> continue on captured context; this will ensure that the
-	/// Synchronization Context returns to the calling thread. If set to <c>false</c> continue
-	/// on a different context; this will allow the Synchronization Context to continue on a
-	/// different thread.
-	/// </param>
-	/// <param name="onException">
-	/// If an exception is thrown in the <see cref="Task" />, <c>onException</c> will execute.
-	/// If onException is null, the exception will be re-thrown.
-	/// </param>
-	/// <remarks>Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.</remarks>
-	/// <exception cref="ArgumentNullException">task</exception>
-	public static async void SafeFireAndForgetAsync(this Task task, bool continueOnCapturedContext = true, Action<Exception>? onException = null)
-	{
-		try
-		{
-			_ = task ?? throw new ArgumentNullException(nameof(task));
-			await task.ConfigureAwait(continueOnCapturedContext);
-		}
-		catch (Exception ex) when (onException is not null)
-		{
-			onException.Invoke(ex);
-		}
-	}
+            await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (TException ex) when (onException is not null)
+        {
+            onException!(ex);
+        }
+    }
+
+    /// <summary>
+    /// Safely execute the <see cref="Task"/> without waiting for it to complete before moving to
+    /// the next line of code; commonly known as "Fire And Forget".
+    /// </summary>
+    /// <param name="task">The <see cref="Task"/>.</param>
+    /// <param name="continueOnCapturedContext">
+    /// If set to <c>true</c> continue on captured context; this will ensure that the
+    /// Synchronization Context returns to the calling thread. If set to <c>false</c> continue on a
+    /// different context; this will allow the Synchronization Context to continue on a different thread.
+    /// </param>
+    /// <param name="onException">
+    /// If an exception is thrown in the <see cref="Task"/>, <c>onException</c> will execute. If
+    /// onException is null, the exception will be re-thrown.
+    /// </param>
+    /// <remarks>Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.</remarks>
+    /// <exception cref="ArgumentNullException">task</exception>
+    public static async void SafeFireAndForgetAsync(this Task task, bool continueOnCapturedContext = true, Action<Exception>? onException = null)
+    {
+        try
+        {
+#if NET6_0_OR_GREATER
+            ArgumentNullException.ThrowIfNull(task);
+#else
+            if (task is null)
+            {
+                throw new ArgumentNullException(nameof(task));
+            }
+#endif
+
+            await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (Exception ex) when (onException is not null)
+        {
+            onException!(ex);
+        }
+    }
 }
--- SharedCode.Core/Threading/Tasks/ValueTaskExtensions.cs
+++ SharedCode.Core/Threading/Tasks/ValueTaskExtensions.cs
@@ -1,40 +1,110 @@
 ﻿namespace SharedCode.Threading.Tasks;
 
 /// <summary>
-/// The extension methods class for <see cref="ValueTask" />.
+/// The extension methods class for <see cref="ValueTask"/>.
 /// </summary>
+[SuppressMessage("Usage", "GCop500:Void async methods cannot be awaited. Also they can hide exceptions and cause tricky bugs. Return a Task instead.", Justification = "The whole point is to fire and forget without await.")]
 public static class ValueTaskExtensions
 {
-	/// <summary>
-	/// Safely execute the <see cref="ValueTask" /> without waiting for it to complete before
-	/// moving to the next line of code; commonly known as "Fire And Forget".
-	/// </summary>
-	/// <typeparam name="TException">The type of exception.</typeparam>
-	/// <param name="task">The <see cref="ValueTask" />.</param>
-	/// <param name="continueOnCapturedContext">
-	/// If set to <c>true</c> continue on captured context; this will ensure that the
-	/// Synchronization Context returns to the calling thread. If set to <c>false</c> continue
-	/// on a different context; this will allow the Synchronization Context to continue on a
-	/// different thread.
-	/// </param>
-	/// <param name="onException">
-	/// If an exception is thrown in the <see cref="ValueTask" />, <c>onException</c> will
-	/// execute. If onException is null, the exception will be re-thrown.
-	/// </param>
-	/// <remarks>Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.</remarks>
-	public static async void SafeFireAndForgetAsync<TException>(
-		this ValueTask task,
-		bool continueOnCapturedContext = true,
-		Action<TException>? onException = null)
-		where TException : Exception
-	{
-		try
-		{
-			await task.ConfigureAwait(continueOnCapturedContext);
-		}
-		catch (TException ex) when (onException is not null)
-		{
-			onException.Invoke(ex);
-		}
-	}
+    /// <summary>
+    /// Safely execute the <see cref="ValueTask"/> without waiting for it to complete before moving
+    /// to the next line of code; commonly known as "Fire And Forget".
+    /// </summary>
+    /// <typeparam name="TException">The type of exception.</typeparam>
+    /// <param name="task">The <see cref="ValueTask"/>.</param>
+    /// <param name="continueOnCapturedContext">
+    /// If set to <c>true</c> continue on captured context; this will ensure that the
+    /// Synchronization Context returns to the calling thread. If set to <c>false</c> continue on a
+    /// different context; this will allow the Synchronization Context to continue on a different thread.
+    /// </param>
+    /// <param name="onException">
+    /// If an exception is thrown in the <see cref="ValueTask"/>, <c>onException</c> will execute.
+    /// If onException is null, the exception will be re-thrown.
+    /// </param>
+    /// <remarks>Inspired by John Thiriet's blog post, "Removing Async Void": https://johnthiriet.com/removing-async-void/.</remarks>
+    public static async void SafeFireAndForgetAsync<TException>(
+        this ValueTask task,
+        bool continueOnCapturedContext = true,
+        Action<TException>? onException = null)
+        where TException : Exception
+    {
+        try
+        {
+            await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (TException ex) when (onException is not null)
+        {
+            onException!(ex);
+        }
+    }
+
+    /// <summary>
+    /// Safely execute the <see cref="ValueTask"/> without waiting for it to complete before moving
+    /// to the next line of code; commonly known as "Fire And Forget".
+    /// </summary>
+    /// <param name="task">The <see cref="ValueTask"/>.</param>
+    /// <param name="continueOnCapturedContext">Whether to continue on captured context.</param>
+    /// <param name="onException">Optional exception handler.</param>
+    public static async void SafeFireAndForgetAsync(
+        this ValueTask task,
+        bool continueOnCapturedContext = true,
+        Action<Exception>? onException = null)
+    {
+        try
+        {
+            await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (Exception ex) when (onException is not null)
+        {
+            onException!(ex);
+        }
+    }
+
+    /// <summary>
+    /// Safely execute the <see cref="ValueTask{TResult}"/> without waiting for it to complete
+    /// before moving to the next line of code; commonly known as "Fire And Forget".
+    /// </summary>
+    /// <typeparam name="TException">The type of exception.</typeparam>
+    /// <typeparam name="TResult">The result type.</typeparam>
+    /// <param name="task">The <see cref="ValueTask{TResult}"/>.</param>
+    /// <param name="continueOnCapturedContext">Whether to continue on captured context.</param>
+    /// <param name="onException">Optional exception handler.</param>
+    public static async void SafeFireAndForgetAsync<TException, TResult>(
+        this ValueTask<TResult> task,
+        bool continueOnCapturedContext = true,
+        Action<TException>? onException = null)
+        where TException : Exception
+    {
+        try
+        {
+            _ = await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (TException ex) when (onException is not null)
+        {
+            onException!(ex);
+        }
+    }
+
+    /// <summary>
+    /// Safely execute the <see cref="ValueTask{TResult}"/> without waiting for it to complete
+    /// before moving to the next line of code; commonly known as "Fire And Forget".
+    /// </summary>
+    /// <typeparam name="TResult">The result type.</typeparam>
+    /// <param name="task">The <see cref="ValueTask{TResult}"/>.</param>
+    /// <param name="continueOnCapturedContext">Whether to continue on captured context.</param>
+    /// <param name="onException">Optional exception handler.</param>
+    public static async void SafeFireAndForgetAsync<TResult>(
+        this ValueTask<TResult> task,
+        bool continueOnCapturedContext = true,
+        Action<Exception>? onException = null)
+    {
+        try
+        {
+            _ = await task.ConfigureAwait(continueOnCapturedContext);
+        }
+        catch (Exception ex) when (onException is not null)
+        {
+            onException!(ex);
+        }
+    }
 }
