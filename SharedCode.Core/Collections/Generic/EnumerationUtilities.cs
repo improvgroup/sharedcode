8514232960120d2971e2d503650231a2f817374f
William Forney <william@williamforney.com>
2025-08-20 22:51:07 -0700

    Refactor EnumerationUtilities.ToList method
    Removed unnecessary using directive and improved the
    ToList method to enforce Enum constraint on type T.
    Updated implementation to use Enum.GetValues<T>()
    for .NET 6 compatibility, simplifying value retrieval
    and enhancing type safety. Added conditional compilation
    to support earlier .NET versions.

--- SharedCode.Core/Collections/Generic/EnumerationUtilities.cs
+++ SharedCode.Core/Collections/Generic/EnumerationUtilities.cs
@@ -1,40 +1,29 @@
-﻿using SharedCode.Properties;
-
-namespace SharedCode.Collections.Generic;
+﻿namespace SharedCode.Collections.Generic;
 
 /// <summary>
 /// The enumeration utilities class.
 /// </summary>
 public static class EnumerationUtilities
 {
-	/// <summary>
-	/// Returns a list of the values in the specified enumeration.
-	/// </summary>
-	/// <typeparam name="T">The type of the enumeration.</typeparam>
-	/// <returns>The list of the values in the specified enumeration.</returns>
-	/// <exception cref="ArgumentException">
-	/// The type of the specified enumeration must be derived from the base <see cref="Enum"/> structure.
-	/// </exception>
-	[SuppressMessage("Refactoring", "GCop659:Use 'var' instead of explicit type.", Justification = "Inference does not resolve int properly here.")]
-	public static IList<T> ToList<T>() where T : struct
-	{
-		var enumType = typeof(T);
+    /// <summary>
+    /// Returns a list of the values in the specified enumeration.
+    /// </summary>
+    /// <typeparam name="T">The type of the enumeration.</typeparam>
+    /// <returns>The list of the values in the specified enumeration.</returns>
+    [SuppressMessage("Refactoring", "GCop659:Use 'var' instead of explicit type.", Justification = "It is a non-generic array so the type cannot be inferred using var.")]
+    public static IList<T> ToList<T>() where T : struct, Enum
+    {
+#if NET6_0_OR_GREATER
+        return [.. Enum.GetValues<T>()];
+#else
+        var values = Enum.GetValues(typeof(T));
+        var list = new List<T>(values.Length);
+        foreach (T value in values)
+        {
+            list.Add(value);
+        }
 
-		// Cannot use type constraints on value types, so have to do check like this.
-		if (enumType.BaseType != typeof(Enum))
-		{
-			throw new ArgumentException(Resources.TheTypeOfTMustBeOfTypeSystemEnum);
-		}
-
-		var enumValuesArray = Enum.GetValues(enumType);
-
-		var enumValuesList = new List<T>(enumValuesArray.Length);
-
-		foreach (int val in enumValuesArray)
-		{
-			enumValuesList.Add((T)Enum.Parse(enumType, val.ToString(CultureInfo.InvariantCulture)));
-		}
-
-		return enumValuesList;
-	}
+        return list;
+#endif
+    }
 }
