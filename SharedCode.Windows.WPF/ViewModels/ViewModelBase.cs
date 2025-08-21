37faa466f409e9617a32a98e41385b4d340383cd
William Forney <william@williamforney.com>
2025-08-20 22:52:02 -0700

    Refactor ViewModelBase and improve documentation
    - Updated `using` directives: removed `SharedCode.Windows.WPF` and added `SharedCode.Linq`.
    - Reformatted and expanded XML documentation for `RunCommandAsync` for clarity.
    - Simplified logic in `RunCommandAsync` to improve readability while maintaining functionality.
    - Enhanced comments and formatting for better understanding and maintainability.

--- SharedCode.Windows.WPF/ViewModels/ViewModelBase.cs
+++ SharedCode.Windows.WPF/ViewModels/ViewModelBase.cs
@@ -1,45 +1,43 @@
-﻿namespace SharedCode.Windows.WPF.ViewModels;
+﻿using SharedCode.Linq;
 
-using SharedCode.Linq;
-using SharedCode.Windows.WPF;
-
-using System;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
+
+namespace SharedCode.Windows.WPF.ViewModels;
 
 /// <summary>
 /// An abstract view model class.
 /// </summary>
 public abstract class ViewModelBase : BindableBase
 {
-	/// <summary>
-	/// Runs a command if the updating flag is not set.
-	/// If the flag is true, indicating the function is already running, then the action is not run.
-	/// If the flag is false, indicating the function is not running, then the action is run.
-	/// Once the action is finished, if it was run, then the flag is reset to false.
-	/// </summary>
-	/// <param name="updatingFlag">The boolean value indicating whether the function is running.</param>
-	/// <param name="action">The action to run, if it is not already running.</param>
-	/// <returns>A <see cref="Task"/>.</returns>
-	/// <exception cref="ArgumentNullException">action</exception>
-	protected static async Task RunCommandAsync(Expression<Func<bool>> updatingFlag, Func<Task> action)
-	{
-		_ = action ?? throw new ArgumentNullException(nameof(action));
+    /// <summary>
+    /// Runs a command if the updating flag is not set. If the flag is true, indicating the function
+    /// is already running, then the action is not run. If the flag is false, indicating the
+    /// function is not running, then the action is run. Once the action is finished, if it was run,
+    /// then the flag is reset to false.
+    /// </summary>
+    /// <param name="updatingFlag">The boolean value indicating whether the function is running.</param>
+    /// <param name="action">The action to run, if it is not already running.</param>
+    /// <returns>A <see cref="Task"/>.</returns>
+    /// <exception cref="ArgumentNullException">action</exception>
+    protected static async Task RunCommandAsync(Expression<Func<bool>> updatingFlag, Func<Task> action)
+    {
+        _ = action ?? throw new ArgumentNullException(nameof(action));
 
-		if (updatingFlag is null || updatingFlag.GetPropertyValue())
-		{
-			return;
-		}
+        // If flag is null or already true, do nothing
+        if (updatingFlag?.GetPropertyValue() != false)
+        {
+            return;
+        }
 
-		updatingFlag.SetPropertyValue(true);
+        updatingFlag.SetPropertyValue(true);
 
-		try
-		{
-			await action().ConfigureAwait(true);
-		}
-		finally
-		{
-			updatingFlag.SetPropertyValue(false);
-		}
-	}
+        try
+        {
+            await action().ConfigureAwait(true);
+        }
+        finally
+        {
+            updatingFlag.SetPropertyValue(false);
+        }
+    }
 }
