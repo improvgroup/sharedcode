cf9bd33c0f97ff1d75f6128df4d0cd72e07e992d
William Forney <william@williamforney.com>
2025-08-20 22:51:47 -0700

    Refactor CommandRepository for clarity and consistency
    Reordered namespace declaration and updated XML documentation for consistency.
    Refactored method implementations in the ICommandRepository interface to use pattern matching, simplifying type checks and enhancing readability.
    Improved exception handling syntax for type mismatches.
    These changes enhance code readability, maintainability, and consistency while preserving existing functionality.

--- SharedCode.Data/CommandRepository.cs
+++ SharedCode.Data/CommandRepository.cs
@@ -1,124 +1,112 @@
-﻿
+﻿using SharedCode.Models;
+
 namespace SharedCode.Data;
 
-using SharedCode.Models;
-
-using System.Collections.Generic;
-using System.Linq;
-
 /// <summary>
-/// The command repository class. Implements the <see cref="ICommandRepository{TEntity}" />.
+/// The command repository class. Implements the <see cref="ICommandRepository{TEntity}"/>.
 /// </summary>
 /// <typeparam name="TEntity">The type of the t entity.</typeparam>
-/// <seealso cref="ICommandRepository{TEntity}" />
+/// <seealso cref="ICommandRepository{TEntity}"/>
 public abstract class CommandRepository<TEntity> : ICommandRepository<TEntity> where TEntity : Entity
 {
-	/// <inheritdoc />
-	public abstract int Add(TEntity entity);
+    /// <inheritdoc/>
+    public abstract int Add(TEntity entity);
 
-	/// <inheritdoc />
-	public abstract IEnumerable<int> Add(IEnumerable<TEntity> entities);
+    /// <inheritdoc/>
+    public abstract IEnumerable<int> Add(IEnumerable<TEntity> entities);
 
-	/// <inheritdoc />
-	int ICommandRepository.Add(Entity entity)
-	{
-		return entity.GetType() == typeof(TEntity)
-			? this.Add((TEntity)entity)
-			: throw new ArgumentException(
-			$"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
-	}
+    /// <inheritdoc/>
+    int ICommandRepository.Add(Entity entity)
+    {
+        return entity is TEntity typed
+            ? this.Add(typed)
+            : throw new ArgumentException($"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
+    }
 
-	/// <inheritdoc />
-	IEnumerable<int> ICommandRepository.Add(IEnumerable<Entity> entities)
-	{
-		return entities is IEnumerable<TEntity>
-			? this.Add(entities.Cast<TEntity>())
-			: throw new ArgumentException(
-			$"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
-	}
+    /// <inheritdoc/>
+    IEnumerable<int> ICommandRepository.Add(IEnumerable<Entity> entities)
+    {
+        return entities is IEnumerable<TEntity> typed
+            ? this.Add(typed)
+            : throw new ArgumentException($"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
+    }
 
-	/// <inheritdoc />
-	public abstract IAddOrUpdateDescriptor AddOrUpdate(TEntity entity);
+    /// <inheritdoc/>
+    public abstract IAddOrUpdateDescriptor AddOrUpdate(TEntity entity);
 
-	/// <inheritdoc />
-	public abstract IEnumerable<IAddOrUpdateDescriptor> AddOrUpdate(IEnumerable<TEntity> entities);
+    /// <inheritdoc/>
+    public abstract IEnumerable<IAddOrUpdateDescriptor> AddOrUpdate(IEnumerable<TEntity> entities);
 
-	/// <inheritdoc />
-	IAddOrUpdateDescriptor ICommandRepository.AddOrUpdate(Entity entity)
-	{
-		return entity.GetType() == typeof(TEntity)
-			? this.AddOrUpdate((TEntity)entity)
-			: throw new ArgumentException(
-			$"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
-	}
+    /// <inheritdoc/>
+    IAddOrUpdateDescriptor ICommandRepository.AddOrUpdate(Entity entity)
+    {
+        return entity is TEntity typed
+            ? this.AddOrUpdate(typed)
+            : throw new ArgumentException($"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
+    }
 
-	/// <inheritdoc />
-	IEnumerable<IAddOrUpdateDescriptor> ICommandRepository.AddOrUpdate(IEnumerable<Entity> entities)
-	{
-		return entities is IEnumerable<TEntity>
-			? this.AddOrUpdate(entities.Cast<TEntity>())
-			: throw new ArgumentException(
-			$"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
-	}
+    /// <inheritdoc/>
+    IEnumerable<IAddOrUpdateDescriptor> ICommandRepository.AddOrUpdate(IEnumerable<Entity> entities)
+    {
+        return entities is IEnumerable<TEntity> typed
+            ? this.AddOrUpdate(typed)
+            : throw new ArgumentException($"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
+    }
 
-	/// <inheritdoc />
-	public abstract bool Remove(int id);
+    /// <inheritdoc/>
+    public abstract bool Remove(int id);
 
-	/// <inheritdoc />
-	public abstract bool Remove(TEntity entity);
+    /// <inheritdoc/>
+    public abstract bool Remove(TEntity entity);
 
-	/// <inheritdoc />
-	public abstract IDictionary<int, bool> Remove(IEnumerable<TEntity> entities);
+    /// <inheritdoc/>
+    public abstract IDictionary<int, bool> Remove(IEnumerable<TEntity> entities);
 
-	/// <inheritdoc />
-	bool ICommandRepository.Remove(Entity entity)
-	{
-		return entity.GetType() == typeof(TEntity)
-			? this.Remove((TEntity)entity)
-			: throw new ArgumentException(
-			$"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
-	}
+    /// <inheritdoc/>
+    bool ICommandRepository.Remove(Entity entity)
+    {
+        return entity is TEntity typed
+            ? this.Remove(typed)
+            : throw new ArgumentException($"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
+    }
 
-	/// <inheritdoc />
-	IDictionary<int, bool> ICommandRepository.Remove(IEnumerable<Entity> entities)
-	{
-		return entities is IEnumerable<TEntity>
-			? this.Remove(entities.Cast<TEntity>())
-			: throw new ArgumentException(
-			$"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
-	}
+    /// <inheritdoc/>
+    IDictionary<int, bool> ICommandRepository.Remove(IEnumerable<Entity> entities)
+    {
+        return entities is IEnumerable<TEntity> typed
+            ? this.Remove(typed)
+            : throw new ArgumentException($"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
+    }
 
-	/// <inheritdoc />
-	public abstract void Update(TEntity entity);
+    /// <inheritdoc/>
+    public abstract void Update(TEntity entity);
 
-	/// <inheritdoc />
-	public abstract void Update(IEnumerable<TEntity> entities);
+    /// <inheritdoc/>
+    public abstract void Update(IEnumerable<TEntity> entities);
 
-	/// <inheritdoc />
-	void ICommandRepository.Update(Entity entity)
-	{
-		if (entity.GetType() == typeof(TEntity))
-		{
-			this.Update((TEntity)entity);
-		}
-		else
-		{
-			throw new ArgumentException(
-				$"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
-		}
-	}
+    /// <inheritdoc/>
+    void ICommandRepository.Update(Entity entity)
+    {
+        if (entity is TEntity typed)
+        {
+            this.Update(typed);
+        }
+        else
+        {
+            throw new ArgumentException($"The type \"{entity.GetType()}\" does not match the type \"{typeof(TEntity)}\"");
+        }
+    }
 
-	/// <inheritdoc />
-	void ICommandRepository.Update(IEnumerable<Entity> entities)
-	{
-		if (entities is IEnumerable<TEntity>)
-		{
-			this.Update(entities.Cast<TEntity>());
-		}
-		else
-		{
-			throw new ArgumentException(
-				$"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
-		}
-	}
+    /// <inheritdoc/>
+    void ICommandRepository.Update(IEnumerable<Entity> entities)
+    {
+        if (entities is IEnumerable<TEntity> typed)
+        {
+            this.Update(typed);
+        }
+        else
+        {
+            throw new ArgumentException($"The type \"{entities.GetType()}\" does not match the type \"{typeof(IEnumerable<TEntity>)}\"");
+        }
+    }
 }
